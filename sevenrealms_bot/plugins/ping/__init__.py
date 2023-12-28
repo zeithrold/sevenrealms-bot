3f600ec5103f3603d083ebab3920247e59ec0dfe
Zeithrold <zeithrold@gmail.com>
2023-12-28 08:17:37 +0000

    removed unnecessary print

--- sevenrealms_bot/plugins/ping/__init__.py
+++ sevenrealms_bot/plugins/ping/__init__.py
@@ -6,8 +6,6 @@ matcher = on_command("ping")
 
 @matcher.handle()
 async def _(bot: Bot, event: GroupMessageEvent):
-    print(event)
-    print(bot)
     group_id = event.group_id
     reply = MessageSegment.at(event.user_id) + MessageSegment.text(" pong")
     await bot.send_group_msg(group_id=group_id, message=reply)
